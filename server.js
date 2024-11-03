61f684a3f6b6c9491e249dcbc8d1aa7c1330f086
Madhan <120784875+madhanmaaz@users.noreply.github.com>
2024-11-03 23:14:30 +0530

    new

--- server.js
+++ server.js
@@ -1,27 +1,8 @@
-const express = require("express")
-const app = express()
-const saturjs = require("saturjs")
+const { setup, Router, renderPage } = require("saturjs")
+const router = Router()
 
-saturjs.setup(app, {
-    dev: true, // just change true in production.
-    version: require("./package.json").version // version of your app.
-})
-app.use(express.static(__dirname + "/public"))
+router.get("/", renderPage("index"))
 
-app.get("/", (req, res) => {
-    const systemInfo = {
-        platform: process.platform,              // OS Platform (e.g., 'linux', 'win32', 'darwin')
-        architecture: process.arch,              // CPU architecture (e.g., 'x64', 'arm')
-        nodeVersion: process.version,            // Node.js version
-        saturjs: 'v1.0.0'
-    }
-
-    res.render("home", {
-        systemInfo
-    })
-})
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log("SaturJs Running on: http://localhost:" + PORT)
+module.exports = setup({
+    appRouter: router,
 })
